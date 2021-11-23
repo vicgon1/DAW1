8990228d7bd2b01d5094ca5697c38407d3e17020
xicasal <91273588+xicasal@users.noreply.github.com>
2021-11-23 19:46:01 +0000

    Add files via upload
    Comienzo de privilegios login

--- Proy Final/js/app.js
+++ Proy Final/js/app.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
     
     //Función de validación de correo de la ulpgc
-    $("#botonLogin").click(function () { 
-        var regex = /^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$"/;
-        if(regex.test($("#correoValido").val().trim())){
-            alert("Correo válido.")
-        }else{
-            alert("El correo tiene que ser de la ulpgc.")
-        }
+    $("#masInfoIngTeleco").click(function () { 
+        $("#infoIngTeleco").show();
     });
 
+
+    $("#contact")
+
 });
+
